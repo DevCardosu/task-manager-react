3444ecff3c3286ffd9d69fd61f6a71119584442b
GUSTAVO JOÃO CARDOSO <gustavo_j_cardoso@estudante.sesisenai.org.br>
2025-11-13 16:06:44 -0300

    Melhorar layout e espaçamento do botão Adicionar

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,51 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [newTask , setNewTask] = useState('')
-  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-  function onWriteTask (event) {
+  function onWriteTask(event) {
     setNewTask(event.target.value);
   }
 
-  function addTask () {
+  function addTask() {
     if (newTask.trim() !== '') {
       setTasks([...tasks, newTask]);
       setNewTask('');
     }
   }
-  
+
+  function deleteTask(indexParaDeletar) {
+    const tarefasFiltradas = tasks.filter((_, index) => index !== indexParaDeletar);
+    setTasks(tarefasFiltradas);
+  }
+
   return (
     <>
       <div className='container'>
         <h1>Gerenciador de Tarefas</h1>
 
-        <input type="text" id='input-task' placeholder='Adicione a tarefa' value={newTask} onChange={onWriteTask} />
+        <input
+          type="text"
+          id='input-task'
+          placeholder='Adicione a tarefa'
+          value={newTask}
+          onChange={onWriteTask}
+        />
 
-        <button id='add-task-button' onClick={addTask}>Adicionar Tarefa</button>
-        
+        <button id='add-task-button' onClick={addTask}>
+          Adicionar
+        </button>
+
         <ul className='card' id='task-list'>
-          {tasks.map((tasks, index) => (
-            <li key={index}>{tasks}</li>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span>{task}</span>
+              <button className='btn-delete' onClick={() => deleteTask(index)}>
+                Deletar
+              </button>
+            </li>
           ))}
-          
         </ul>
       </div>
     </>
